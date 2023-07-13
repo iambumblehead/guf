8409058d03cb396512186b6b55ebde1f1dfe9af6
chris <chris@bumblehead.com>
2023-07-12 23:55:15 -0700

    use sorting by filetype then by filename

--- guf/gufsort.scm
+++ guf/gufsort.scm
@@ -1,21 +1,23 @@
 (define-module (guf gufsort)
-  #:use-module (ice-9 regex)
   #:use-module (guf guffile)
   #:export (gufsortlist
             gufsortfilelist))
 
-(define (asalphanum str)
-  (regexp-substitute/global #f "[^[:alnum:]]" str 'pre "" 'post))
+(define (gufsort-filenamealnum left right)
+  (string<? (guffile-getfilenamealnum left)
+            (guffile-getfilenamealnum right)))
 
-(define (sortalphanum left right)
-  (string<? (asalphanum left) (asalphanum right)))
-  
-(define (gufsortlist items)
-  (sort items sortalphanum))
+(define (gufsort-filetypes-same? left right)
+  (eq? (guffile-getfiletype left)
+       (guffile-getfiletype right)))
 
-(define (sortfilealphanum left right)
-  (sortalphanum (guffile-getfilename left)
-                (guffile-getfilename right)))
+(define (gufsort-filetypes left right)
+  (if (eq? (guffile-getfiletype left) 'directory) #t #f))
+
+(define (sortdirfilealphanum left right)
+  (if (gufsort-filetypes-same? left right)
+      (gufsort-filenamealnum left right)
+      (gufsort-filetypes left right)))
 
 (define (gufsortfilelist items)
-  (sort items sortfilealphanum))
+  (sort items sortdirfilealphanum))
